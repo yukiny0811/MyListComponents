6685d7dd1aeb5ffe12ab746da2edb87594fecd07
broccoli3 <pocomai.3315@gmail.com>
2024-11-09 17:32:41 +0900

    アラーム機能のリストを作りました

--- MyListComponents/MyListComponentsApp.swift
+++ MyListComponents/MyListComponentsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyListComponentsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(isAlarmOn: Bool())
         }
     }
 }
