b02289fe439ee38f7288802817916ec2d10439fd
yukiny0811 <yukiny0811@gmail.com>
2024-11-09 13:00:24 +0900

    Initial Commit

---
+++ MyListComponents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyListComponents
+//
+//  Created by Yuki Kuwashima on 2024/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyListComponents/MyListComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListComponentsApp.swift
+//  MyListComponents
+//
+//  Created by Yuki Kuwashima on 2024/11/09.
+//
+
+import SwiftUI
+
+@main
+struct MyListComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
